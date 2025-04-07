69667fc13e3daaa77cca6e7dc8fc48094798e65e
cdunn-dev <dunn.chris.r@gmail.com>
2025-04-07 13:37:41 +0100

    fix: resolve test configuration issues

--- packages/shared/jest.config.js
+++ packages/shared/jest.config.js
@@ -3,11 +3,18 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testMatch: ['**/__tests__/**/*.+(spec|test).ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: ['/node_modules/', 'setup.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/src/__tests__/setup.ts'
+  ],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
